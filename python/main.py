de240e6a76f5f7826833f8f9d016e75ffea6ca83
Linus Stoltz <lstoltz5@gmail.com>
2025-04-04 22:29:34 -0400

    update version

--- python/main.py
+++ python/main.py
@@ -1,7 +1,7 @@
 __author__ = 'Linus Stoltz | Data Manager, CFRF'
 __project_team__ = 'Linus Stoltz, Sarah Salois, George Maynard, Mike Morin'
 __doc__ = 'FIShBOT program to aggregate regional data collected by NOAA and CFRF'
-__version__ = '0.1'
+__version__ = '0.2'
 
 import logging
 from logging.handlers import TimedRotatingFileHandler
